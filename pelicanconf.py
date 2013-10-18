1bc3b8e83c475f9e7322cd07cd51885db8de26ea
Richard Gray <richard@crash.net.nz>
2013-10-18 21:21:54 +0100

    Initial commit of Pelican site

---
+++ pelicanconf.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- #
+from __future__ import unicode_literals
+
+AUTHOR = u'Richard Gray'
+SITENAME = u'crash.net.nz'
+SITEURL = ''
+
+TIMEZONE = 'Europe/London'
+
+DEFAULT_LANG = u'en'
+
+# Feed generation is usually not desired when developing
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+
+# Blogroll
+LINKS =  (('Pelican', 'http://getpelican.com/'),
+          ('Python.org', 'http://python.org/'),
+          ('Jinja2', 'http://jinja.pocoo.org/'))
+
+# Social widget
+SOCIAL = (('Twitter', 'https://twitter.com/crashnetnz'),
+          ('Github', 'https://github.com/vortura'),
+          ('Last.fm', 'http://www.last.fm/user/vortura'),
+          ('Flickr', 'https://secure.flickr.com/photos/richardgray/'),)
+
+DEFAULT_PAGINATION = False
+
+# Uncomment following line if you want document-relative URLs when developing
+#RELATIVE_URLS = True
